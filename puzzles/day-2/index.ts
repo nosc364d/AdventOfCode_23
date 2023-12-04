d0422c5cf6ee349950477d3e9f8f0226aedc5620
nrs <nora.schaefer@mailbox.tu-dresden.de>
2023-12-05 00:13:54 +0100

    solve day 2 pt 1

--- puzzles/day-2/index.ts
+++ puzzles/day-2/index.ts
@@ -7,15 +7,51 @@ export async function task1() {
   // 1 line of data looks like this:
   // game {id}: ({#cubes} {color})+
 
-  const digit = new RegExp("([0-9])"); // like this it will just get the first match -> great for gettign the IDs
+  // init data i guess
+  const colors = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  };
 
-  lines.map((line) => {
-    if (line !== "") {
-      const id = digit.exec(line)[0];
+  var sum = 0;
+
+  lines.map((game) => {
+    if (game !== "") {
+      // init data
+      const sets = game.split("; ");
+
+      var gameOk = true;
+      var id = 0;
+      // TODO: refactor cuz a lot of code duplication
+      sets.map((set, setIndex) => {
+        const tries = set.split(", "); // tries = [ "game 98: n color", "m color" , ... ]
+        tries.map((cubes, cubeIndex) => {
+          const elements = cubes.split(" "); // elements = [ "game", "id:", "n", "color"] | [ "m", "color"]
+
+          if (setIndex + cubeIndex === 0) {
+            id = Number(elements[1].replace(":", ""));
+          }
+          const number = Number(elements[cubeIndex + setIndex === 0 ? 2 : 0]);
+          colors[elements[cubeIndex + setIndex === 0 ? 3 : 1]] += number;
+        });
+
+        if (colors.red > 12 || colors.green > 13 || colors.blue > 14) {
+          gameOk = false; // entire game is fucked, if those numbers dont align
+        }
+        // reset object for next set
+        colors.red = 0;
+        colors.green = 0;
+        colors.blue = 0;
+      });
+
+      if (gameOk) {
+        sum += Number(id);
+      }
     }
   });
 
-  console.log(digit.exec(lines[0]));
+  console.log(sum);
 }
 
 task1();
