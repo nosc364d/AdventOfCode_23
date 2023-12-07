858ce1b5284c8f3ecea6595db8b5336597e10343
nrs <nora.schaefer@mailbox.tu-dresden.de>
2023-12-07 15:22:14 +0100

    solve day 5 pt 1 and lay down structure for day 2 but gotta optimize first

---
+++ puzzles/day-5/index.ts
@@ -0,0 +1,129 @@
+import { getData } from "../../lib/data";
+
+function isInRange(toTest: number, start: number, end: number): boolean {
+  console.log("testing: [" + start + ", " + toTest + ", " + end + "]");
+  return start <= toTest && toTest <= end;
+}
+
+// export async function task1() {
+//   const data = await getData("5");
+
+//   // const testData =
+//   //   "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4\n";
+
+//   const seedRegex = /^seeds:\s(?<seed>.+)/; // regex syntax copied from tlareg on github
+
+//   const seeds = data.match(seedRegex)[1].split(" ");
+//   var seedsAsNums: number[] = [];
+//   seeds.map((seed) => {
+//     seedsAsNums.push(Number(seed));
+//   });
+
+//   console.log(seeds);
+
+//   const lines = data.split("\n");
+
+//   var check = true;
+
+//   // for every seed, go through every line and do all the mappings until location
+//   for (var seedIndex = 0; seedIndex < seedsAsNums.length; seedIndex++) {
+//     lines.map((line) => {
+//       if (line.match(/[0-9]/)) {
+//         // line is a number
+//         const mapping = line.split(" ");
+//         // console.log(mapping);
+//         if (mapping[0] !== "seeds:") {
+//           // assign the mapping results for better readability
+//           // we're not golfing here
+//           const dst = Number(mapping[0]);
+//           const src = Number(mapping[1]);
+//           const range = Number(mapping[2]);
+//           // only if number is in range, do the mapping
+//           if (isInRange(seedsAsNums[seedIndex], src, src + range) && check) {
+//             // console.log("mapping for " + seedsAsNums[seedIndex]);
+//             // console.log("found in range from " + src + " to " + (src + range));
+//             // console.log("now " + (dst + (seedsAsNums[seedIndex] - src)));
+//             seedsAsNums[seedIndex] = dst + (seedsAsNums[seedIndex] - src);
+//             check = false;
+//           }
+//         }
+//       } else {
+//         check = true;
+//       }
+//     });
+//   }
+
+//   console.log(seedsAsNums.sort((a, b) => a - b));
+// }
+
+export async function task2() {
+  const data = await getData("5");
+
+  // const testData =
+  //   "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4\n";
+
+  const seedRegex = /^seeds:\s(?<seed>.+)/; // regex syntax copied from tlareg on github
+
+  const seeds = data.match(seedRegex)[1].split(" ");
+  var seedsAsNums: number[] = [];
+  seeds.map((seed) => {
+    seedsAsNums.push(Number(seed));
+  });
+
+  console.log(seeds);
+
+  const lines = data.split("\n");
+
+  var check = true;
+  var currentLowestResult = seedsAsNums[0];
+
+  // for every seed, go through every line and do all the mappings until location
+  for (var seedIndex = 0; seedIndex < seedsAsNums.length; seedIndex++) {
+    if (seedIndex % 2 === 0) {
+      for (
+        var thisSeeds = 0;
+        thisSeeds < seedsAsNums[seedIndex + 1];
+        thisSeeds++
+      ) {
+        lines.map((line) => {
+          if (line.match(/[0-9]/)) {
+            // line is a number
+            const mapping = line.split(" ");
+            // console.log(mapping);
+            if (mapping[0] !== "seeds:") {
+              // assign the mapping results for better readability
+              // we're not golfing here
+              const dst = Number(mapping[0]);
+              const src = Number(mapping[1]);
+              const range = Number(mapping[2]);
+              // only if number is in range, do the mapping
+              if (
+                isInRange(
+                  seedsAsNums[seedIndex] + thisSeeds,
+                  src,
+                  src + range
+                ) &&
+                check
+              ) {
+                // console.log("mapping for " + seedsAsNums[seedIndex]);
+                // console.log("found in range from " + src + " to " + (src + range));
+                // console.log("now " + (dst + (seedsAsNums[seedIndex] - src)));
+                seedsAsNums[seedIndex] =
+                  dst + (seedsAsNums[seedIndex] + thisSeeds - src);
+                check = false;
+              }
+              if (currentLowestResult > seedsAsNums[seedIndex])
+                currentLowestResult = seedsAsNums[seedIndex];
+            }
+          } else {
+            check = true;
+          }
+        });
+      }
+    }
+  }
+
+  console.log(currentLowestResult);
+}
+
+task2();
