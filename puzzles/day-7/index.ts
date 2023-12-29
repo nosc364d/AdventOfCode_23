4be4b00d1672f6811b5df1040eae95aca9e586e7
nosc364d <nora.schaefer@mailbox.tu-dresden.de>
2023-12-29 11:04:48 +0100

    draft day 7 pt 1

---
+++ puzzles/day-7/index.ts
@@ -0,0 +1,95 @@
+import { getData } from "../../lib/data";
+
+export async function task1() {
+  const data = await getData("7");
+  const testdata = "32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483";
+
+  const mapping = {
+    A: 14,
+    K: 13,
+    Q: 12,
+    J: 11,
+    T: 10,
+  };
+
+  const lines = data.split("\n");
+
+  const kindsOfHands = {
+    five_of_kind: [],
+    four_of_kind: [],
+    full_house: [],
+    three_of_kind: [],
+    two_pair: [],
+    one_pair: [],
+    high_card: [],
+  };
+
+  let numOfHands = lines.length;
+  let result = 0;
+
+  lines.map((line) => {
+    const occurences = {};
+    const handAndBet = line.split(" "); // will remain stored here
+    const hand = handAndBet[0].split("");
+
+    let handWithNums = [];
+
+    hand.map((char) => {
+      if (occurences[char]) {
+        occurences[char]++;
+      } else occurences[char] = 1;
+
+      if (mapping[char]) {
+        handWithNums.push(mapping[char]);
+      } else {
+        handWithNums.push(Number(char));
+      }
+    });
+
+    handWithNums.push(handAndBet[1]);
+
+    if (Object.keys(occurences).length === 5) {
+      // can only be 5 distinct charas
+      kindsOfHands.high_card.push(handWithNums);
+    } else if (Object.keys(occurences).length === 4) {
+      // only 1 can be a pair
+      kindsOfHands.one_pair.push(handWithNums);
+    } else if (Object.keys(occurences).length === 3) {
+      // can be two pair or three of a kind
+      if (Object.values(occurences).includes(3)) {
+        kindsOfHands.three_of_kind.push(handWithNums);
+      } else {
+        kindsOfHands.two_pair.push(handWithNums);
+      }
+    } else if (Object.keys(occurences).length === 2) {
+      if (Object.values(occurences).includes(4)) {
+        kindsOfHands.four_of_kind.push(handWithNums);
+      } else {
+        kindsOfHands.full_house.push(handWithNums);
+      }
+    } else if (Object.keys(occurences).length === 1) {
+      kindsOfHands.five_of_kind.push(handWithNums);
+    }
+  });
+
+  // sort object
+  Object.values(kindsOfHands).map((listOfHands) => {
+    listOfHands.sort();
+  });
+
+  console.log(Object.values(kindsOfHands));
+
+  // make all arrays into 1 and map
+  Object.values(kindsOfHands).map((hands) => {
+    if (hands.length > 0) {
+      hands.map((hand) => {
+        result += numOfHands * Number(hand[5]);
+        numOfHands--;
+      });
+    }
+  });
+
+  console.log(result);
+}
+
+task1();
